30052e2fb619423ba27ee04bf6a619c2bc34a919
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-03 16:56:06 +0100

    change color

--- src/client/app/AboutMe.react.js
+++ src/client/app/AboutMe.react.js
@@ -10,9 +10,9 @@ class AboutMe extends React.Component {
     return (
       <div className="about-me-default-primary-color w3-container">
         <div className="w3-padding-64 w3-center">
-          <h1 className="about-me-primary-text-color" data-translatable>About Me // Super </h1>
+          <h1 className="about-me-text-primary-color" data-translatable>About Me // Super </h1>
           <img src="../../res/profile.jpeg" className="w3-margin w3-circle" alt="Person" style={{ width: "15%" }}/>
-          <div className="about-me-primary-text-color w3-padding-xxlarge">
+          <div className="about-me-text-primary-color w3-padding-xxlarge">
             <p>Lorem ipusm sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
             <p>Lorem ipusm sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
           </div>
