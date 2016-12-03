60a1dd7a89fc3c6be2f3950ca37eaa1a7bb0b625
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-03 14:22:12 +0100

    change colors in resume

--- src/client/app/Resume.react.js
+++ src/client/app/Resume.react.js
@@ -8,56 +8,58 @@ class Resume extends React.Component {
   
   render () {
     return (
-      <div className="w3-indigo w3-container">
+      <div className="resume-default-primary-color w3-container">
         <div className="w3-padding-64 w3-center">
-          <h2>
-            <span data-translatable> Resume // Curricul vitae </span>
-          </h2>
-          <p>
-            <span data-translatable> A draft from my CV // Travail en cours sur mon CV :) </span>
-          </p>
+          <div className="resume-text-primary-color">
+            <h2>
+              <span data-translatable> Resume // Curricul vitae </span>
+            </h2>
+            <p>
+              <span data-translatable> A draft from my CV // Travail en cours sur mon CV :) </span>
+            </p>
+          </div>
           <div className="w3-container w3-responsive">
             <table className="w3-table">
-              <tr>
-                <th>
-                  <span data-translatable> Year // Année </span>
+              <tr className="resume-dark-primary-color">
+                <th className="resume-text-primary-color">
+                  <span data-translatable > Year // Année </span>
                 </th>
-                <th>
+                <th className="resume-text-primary-color">
                   <span data-translatable> Title // Titre </span>
                 </th>
-                <th>
+                <th className="resume-text-primary-color">
                   <span data-translatable>Where // Lieu </span>
                 </th>
               </tr>
-              <tr className="w3-white">
-                <td>08/15 - 01/17</td>
-                <td>Back-end ingineer at <a href="http://applidget.com"> Applidget </a></td>
-                <td>Paris, France</td>
+              <tr className="resume-light-primary-color">
+                <td className="resume-text-primary-color">08/15 - 01/17</td>
+                <td className="resume-text-primary-color">Back-end engineer at <a href="http://applidget.com"> Applidget </a></td>
+                <td className="resume-text-primary-color" >Paris, France</td>
               </tr>
               <tr>
-                <td>02/15 - 07/15</td>
-                <td>3D software programmer internship at <a href="https://masa-group.biz/"> Masa Group</a></td>
-                <td>Paris, France</td>
+                <td className="resume-text-primary-color">02/15 - 07/15</td>
+                <td className="resume-text-primary-color">3D software programmer internship at <a href="https://masa-group.biz/"> Masa Group</a></td>
+                <td className="resume-text-primary-color">Paris, France</td>
               </tr>
-              <tr className="w3-white">
-                <td>09/13 - 02/14</td>
-                <td>Software programmer internship at <a href="http://www.multiposting.fr/fr/"> Multiposting</a></td>
-                <td>Paris, France</td>
+              <tr className="resume-light-primary-color">
+                <td className="resume-text-primary-color">09/13 - 02/14</td>
+                <td className="resume-text-primary-color">Software programmer internship at <a href="http://www.multiposting.fr/fr/"> Multiposting</a></td>
+                <td className="resume-text-primary-color">Paris, France</td>
               </tr>
               <tr>
-                <td>2012 - 2015</td>
-                <td>Master's Degree in Computer Engineering</td>
-                <td>Université de Compiègne, France</td>
+                <td className="resume-text-primary-color">2012 - 2015</td>
+                <td className="resume-text-primary-color">Master's Degree in Computer Engineering</td>
+                <td className="resume-text-primary-color">Université de Compiègne, France</td>
               </tr>
-              <tr className="w3-white">
-                <td>2010 - 2012</td>
-                <td>2-year engineering diploma in Computer Science</td>
-                <td>UIT D'Arles, France</td>
+              <tr className="resume-light-primary-color">
+                <td className="resume-text-primary-color">2010 - 2012</td>
+                <td className="resume-text-primary-color">2-year engineering diploma in Computer Science</td>
+                <td className="resume-text-primary-color">UIT D'Arles, France</td>
               </tr>
               <tr className="w3-hide-medium">
-                <td>2007 - 2010</td>
-                <td>French Baccalauréat</td>
-                <td>Avignon, France</td>
+                <td className="resume-text-primary-color">2007 - 2010</td>
+                <td className="resume-text-primary-color">French Baccalauréat</td>
+                <td className="resume-text-primary-color">Avignon, France</td>
               </tr>
             </table>
           </div>
