6762a0486428d529daab2393d03def7e5ec7b199
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-07 23:26:20 +0100

    use child element to fill articles container

--- src/client/app/ArticlesContainer.react.js
+++ src/client/app/ArticlesContainer.react.js
@@ -1,71 +1,69 @@
 import React from 'react';
 
 import { t } from "./Translations";
+import Article from "./Article.react";
+
+const nbArticles = 5;
 
 class ArticlesContainer extends React.Component {
 
   constructor(props){
     super(props);
+    this.renderPagination = this.renderPagination.bind(this);
+    this.renderChildren = this.renderChildren.bind(this);
   }
 
   renderPagination(){
+    const { children } = this.props;
+    const nodes = children.map((child, index) => {
+      return (<li key={index}><a href="">{index+1}</a></li>);
+    })
     return (
       <div className="row text-center">
         <div className="col-sm-12">
           <ul className="pagination">
-            <li><a href="">«</a></li>
-            <li><a href="">»</a></li>
+            <li key="first"><a href="">«</a></li>
+            {nodes}
+            <li key="last"><a href="">»</a></li>
           </ul>
         </div>
       </div>
     );
   }
 
+  renderChildren() {
+    const { children } = this.props;
+    return children.map((child, index) => {
+      return (
+        <span key={index}>
+          {child}
+          <hr/>
+        </span>
+      );
+    });
+  }
+
   render () {
-    const { locale } = this.props;
+    const { locale, title, children } = this.props;
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <h1 className="page-header"> Page header</h1>
+            <h1 className="page-header">{title}</h1>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-7">
-            <img className="image-responsive" src="http://placehold.it/600x300"/>
-            <a href="#"></a>
-          </div>
-          <div className="col-md-5">
-            <h3> Project One</h3>
-            <h4> Subheading </h4>
-            <p>Subheading fdsjkvjsdv d sv kd jfkdlndsks kdsjdgldsgqslkgjqdklg kgjdkgqkdsljgkldsqgj kjqgkdlgjkldsgjdkgjlsqgjk</p>
-            <a className="btn btn-primary" href="#">
-              View project 
-              <span className="glyphicon glyphicon-chevron-right"></span>
-            </a>
-          </div>
-        </div>
-        <hr/>
-        <div className="row">
-          <div className="col-md-7">
-            <img className="image-responsive" src="http://placehold.it/600x300"/>
-            <a href="#"></a>
-          </div>
-          <div className="col-md-5">
-            <h3> Project Two</h3>
-            <h4> Subheading </h4>
-            <p>Subheading fdsjkvjsdv d sv kd jfkdlndsks kdsjdgldsgqslkgjqdklg kgjdkgqkdsljgkldsqgj kjqgkdlgjkldsgjdkgjlsqgjk</p>
-            <a className="btn btn-primary" href="#">
-              View project 
-              <span className="glyphicon glyphicon-chevron-right"></span>
-            </a>
-          </div>
-        </div>
-        <hr/>
-        
+        {this.renderChildren()}
       </div>
     );
   }
 }
+
+ArticlesContainer.propTypes = {
+  title: React.PropTypes.string
+};
+
+ArticlesContainer.defaultProps = {
+  title: 'Page header'
+};
 
 export default ArticlesContainer;
