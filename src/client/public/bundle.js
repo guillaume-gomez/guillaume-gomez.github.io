9082e1bc1903ae1cf1c25930b689bd22430f6690
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-16 09:46:28 +0100

    fetch image from github

--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -41882,7 +41882,7 @@
 	    student_project_nf11_subtitle: "Travaux pratiques sur la théorie des langages",
 	    student_project_nf11_content: "Le but de se projet était de pratiquer fondamentaux de la théorie des langages. Ce projet contenait un squelette avec un analyseur syntaxique ainsi qu'un outil de build(un projet netbeans). Le projet m'a permis de comprendre les notions vues en cours de maniere ludique(on peut faire pleins de dessins rapidement en Logo :))",
 	    student_project_nf11_buttonText: "Voir le repo",
-	    student_project_nf11_href: "https://github.com/guillaume-gomez/NF11-project/blob/67798b057eea3d7204dd82e8abed0d95544911a6/example.png",
+	    student_project_nf11_href: "https://raw.githubusercontent.com/guillaume-gomez/NF11-project/master/example.png?raw=true",
 	    student_project_nf11_githubUrl: "https://github.com/guillaume-gomez/NF11-project"
 	  },
 	  "en": {
