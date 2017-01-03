477e175fc82045bc4574d3160ae7bb77a871b306
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-03 08:42:57 +0100

    add translation to describe myself

--- src/client/public/bundle.js
+++ src/client/public/bundle.js
@@ -41862,7 +41862,7 @@
 	  value: true
 	});
 	
-	var _fr;
+	var _fr, _en;
 	
 	exports.t = t;
 	
@@ -41885,7 +41885,7 @@
 	    resume_bac: "Baccalauréat scientifique",
 	    resume_programming: "Programmation",
 	    about_me_title: "Quelques mots",
-	    about_me_content: "Lorem ipusm sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+	    about_me_content: "Developpeur depuis environ 10 ans; j'ai codé mes premieres lignes de code en C, pour ensuite decouvrir beaucoup d'autres langages que ce soit à travers mon cursus universitaire ou professionel. Je conçois des outils pour repondre aux besoins des utilisateurs. Passioné par l'aspect creatif que peut être l'informatique, j'essaye à travers des projets personnels d'entrevoir d'autres technologies ainsi que d'autres algorithmes. Je suis par ailleurs un dingue de sport. Idem pour ma musique avec lequel je partage une grande partie de ma vie(au travail, sur le chemin du travail, entre deux seances de sport...). Je laisse donc en lien mon compte senscritque pour parler musique ou autres; en plus des classique github et linkedin :)",
 	    index_about_me: "A propos",
 	    index_resume: "CV",
 	    index_work: "Mes travaux",
@@ -41937,7 +41937,7 @@
 	    personal_project_LD36_buttonText: "Jouer",
 	    personal_project_LD36_href: "https://raw.githubusercontent.com/guillaume-gomez/LD36/master/preview.png"
 	  }, _defineProperty(_fr, "personal_project_LD36_githubUrl", "https://guillaume-gomez.github.io/LD36"), _defineProperty(_fr, "personal_project_LD37_title", "Ludum Dare 37"), _defineProperty(_fr, "personal_project_LD37_subtitle", "Theme : One room"), _defineProperty(_fr, "personal_project_LD37_content", "Dernière participation de l'année 2016. Toujours en phaser avec une chaine de build pour coder en ES6. Le jeu genere une salle randomisé, les ennemies peuvent pousser les blocs de la salle mais pas le hero. Pour gagner le personnage doit soit sortir de la salle(en laissant entrer les enemies), soit tous les eliminer."), _defineProperty(_fr, "personal_project_LD37_buttonText", "Jouer (bientôt)"), _defineProperty(_fr, "personal_project_LD37_href", ""), _defineProperty(_fr, "personal_project_LD37_githubUrl", "https://guillaume-gomez.github.io/LD37"), _defineProperty(_fr, "personal_project_projet_math_title", "Math game"), _defineProperty(_fr, "personal_project_projet_math_subtitle", "Nom définitif à definir :)"), _defineProperty(_fr, "personal_project_projet_math_content", "Le but était ici de faire un jeu vidéo basé sur l'utilisation de concepts mathématiques. Le tout en ajoutant des mécaniques de plateforme 2D. Ce projet est toujours en cours d'amélioration."), _defineProperty(_fr, "personal_project_projet_math_buttonText", "Voir le repo"), _defineProperty(_fr, "personal_project_projet_math_href", "https://raw.githubusercontent.com/guillaume-gomez/MathGame/master/resReadme/in-game3.png"), _defineProperty(_fr, "personal_project_projet_math_githubUrl", "https://guillaume-gomez.github.io/MathGame"), _fr),
-	  "en": {
+	  "en": (_en = {
 	    a: "at",
 	    resume_title: "Resume",
 	    resume_sub_title: "A draft from my CV",
@@ -41963,61 +41963,48 @@
 	    student_project_title: "Student Projects",
 	    personal_project_title: "Personal Projects",
 	    contribution_title: "Contribution",
-	    student_project_rep_code_title: "",
-	    student_project_rep_code_subtitle: "",
+	    student_project_rep_code_title: "Image processing student project",
+	    student_project_rep_code_subtitle: "2D library to generate images",
 	    student_project_rep_code_content: "",
-	    student_project_rep_code_buttonText: "",
+	    student_project_rep_code_buttonText: "see the repository",
 	    student_project_rep_code_href: "",
-	    student_project_rep_code_githubUrl: "",
-	    student_project_nf11_title: "",
-	    student_project_nf11_subtitle: "",
+	    student_project_rep_code_githubUrl: "https://github.com/guillaume-gomez/Rep-Code",
+	    student_project_nf11_title: "Partial logo compiler",
+	    student_project_nf11_subtitle: "Practical work about languages theory",
 	    student_project_nf11_content: "",
-	    student_project_nf11_buttonText: "",
-	    student_project_nf11_href: "",
-	    student_project_nf11_githubUrl: "",
-	    student_project_nf29_title: "",
+	    student_project_nf11_buttonText: "see the repository",
+	    student_project_nf11_href: "https://raw.githubusercontent.com/guillaume-gomez/NF11-project/master/example.png?raw=true",
+	    student_project_nf11_githubUrl: "https://github.com/guillaume-gomez/NF11-project",
+	    student_project_nf29_title: "Development of a MindMapping to Hdoc converter",
 	    student_project_nf29_subtitle: "",
 	    student_project_nf29_content: "",
-	    student_project_nf29_buttonText: "",
-	    student_project_nf29_href: "",
-	    student_project_nf29_githubUrl: "",
-	    participation_project_LD34_title: "",
-	    participation_project_LD34_subtitle: "",
+	    student_project_nf29_buttonText: "see the repository",
+	    student_project_nf29_href: "res/NF29.png",
+	    student_project_nf29_githubUrl: "https://github.com/guillaume-gomez/NF29-XmlConverter",
+	    participation_project_LD34_title: "Ludum Dare 34",
+	    participation_project_LD34_subtitle: "Theme : Two Button Controls",
 	    participation_project_LD34_content: "",
-	    pparticipation_project_LD34_buttonText: "",
-	    participation_project_LD34_href: "",
-	    participation_project_LD34_githubUrl: "",
-	    participation_project_camlCompiler_title: "",
-	    participation_project_camlCompiler_subtitle: "",
+	    pparticipation_project_LD34_buttonText: "see the repository",
+	    participation_project_LD34_href: "res/LD34.png",
+	    participation_project_LD34_githubUrl: "https://github.com/RMEx/LD34",
+	    participation_project_camlCompiler_title: "Caml Compiler",
+	    participation_project_camlCompiler_subtitle: "Ocaml compiler coded in C++",
 	    participation_project_camlCompiler_content: "",
 	    participation_project_camlCompiler_buttonText: "",
-	    participation_project_camlCompiler_href: "",
-	    participation_project_camlCompiler_githubUrl: "",
-	    personal_project_LD35_title: "",
-	    personal_project_LD35_subtitle: "",
+	    participation_project_camlCompiler_href: "https://www.lexifi.com/files/images/csml_winforms_client.jpg",
+	    participation_project_camlCompiler_githubUrl: "https://github.com/AdrienJarretier/CamlCompiler",
+	    personal_project_LD35_title: "Ludum Dare 35",
+	    personal_project_LD35_subtitle: "Theme : Shapeshift",
 	    personal_project_LD35_content: "",
-	    personal_project_LD35_buttonText: "",
-	    personal_project_LD35_href: "",
-	    personal_project_LD35_githubUrl: "",
-	    personal_project_LD36_title: "",
-	    personal_project_LD36_subtitle: "",
+	    personal_project_LD35_buttonText: "Play",
+	    personal_project_LD35_href: "https://raw.githubusercontent.com/guillaume-gomez/LD35/master/css/screen-shot.png",
+	    personal_project_LD36_githubUrl: "https://guillaume-gomez.github.io/LD35",
+	    personal_project_LD36_title: "Ludum Dare 36",
+	    personal_project_LD36_subtitle: "Theme : Ancient Technology",
 	    personal_project_LD36_content: "",
-	    personal_project_LD36_buttonText: "",
-	    personal_project_LD36_href: "",
-	    personal_project_LD36_githubUrl: "",
-	    personal_project_LD37_title: "",
-	    personal_project_LD37_subtitle: "",
-	    personal_project_LD37_content: "",
-	    personal_project_LD37_buttonText: "",
-	    personal_project_LD37_href: "",
-	    personal_project_LD37_githubUrl: "",
-	    personal_project_projet_math_title: "",
-	    personal_project_projet_math_subtitle: "",
-	    personal_project_projet_math_content: "",
-	    personal_project_projet_math_buttonText: "",
-	    personal_project_projet_math_href: "",
-	    personal_project_projet_math_githubUrl: ""
-	  }
+	    personal_project_LD36_buttonText: "Play",
+	    personal_project_LD36_href: "https://raw.githubusercontent.com/guillaume-gomez/LD36/master/preview.png"
+	  }, _defineProperty(_en, "personal_project_LD36_githubUrl", "https://guillaume-gomez.github.io/LD36"), _defineProperty(_en, "personal_project_LD37_title", "Ludum Dare 37"), _defineProperty(_en, "personal_project_LD37_subtitle", "Theme : One room"), _defineProperty(_en, "personal_project_LD37_content", ""), _defineProperty(_en, "personal_project_LD37_buttonText", "Play (soon)"), _defineProperty(_en, "personal_project_LD37_href", ""), _defineProperty(_en, "personal_project_LD37_githubUrl", "https://guillaume-gomez.github.io/LD37"), _defineProperty(_en, "personal_project_projet_math_title", "Math game"), _defineProperty(_en, "personal_project_projet_math_subtitle", ""), _defineProperty(_en, "personal_project_projet_math_content", ""), _defineProperty(_en, "personal_project_projet_math_buttonText", "go to repository"), _defineProperty(_en, "personal_project_projet_math_href", "https://raw.githubusercontent.com/guillaume-gomez/MathGame/master/resReadme/in-game3.png"), _defineProperty(_en, "personal_project_projet_math_githubUrl", "https://guillaume-gomez.github.io/MathGame"), _en)
 	};
 	
 	function t(locale, key) {
@@ -42515,7 +42502,6 @@
 	    value: function render() {
 	      var locale = this.props.locale;
 	
-	      debugger;
 	      return _react2.default.createElement(
 	        _ArticlesContainer2.default,
 	        { locale: locale, title: (0, _Translations.t)(locale, "student_project_title") },
@@ -42999,7 +42985,7 @@
 	          buttonText: (0, _Translations.t)(locale, "participation_project_camlCompiler_buttonText"),
 	          href: (0, _Translations.t)(locale, "participation_project_camlCompiler_href"),
 	          githubUrl: (0, _Translations.t)(locale, "participation_project_camlCompiler_githubUrl"),
-	          languages: "C++, Google Test"
+	          languages: "C++, Google Test, Travis ci"
 	        }),
 	        _react2.default.createElement(_Article2.default, {
 	          title: (0, _Translations.t)(locale, "participation_project_LD34_title"),
