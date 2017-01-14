a2f24726c2aa2c33e04a85b8b47c993058966e06
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-14 03:17:15 +0100

    add articles

--- src/client/app/PersonalProjects.react.js
+++ src/client/app/PersonalProjects.react.js
@@ -50,6 +50,15 @@ class PersonalProjects extends React.Component {
           githubUrl={t(locale, "personal_project_projet_math_githubUrl")}
           languages="C++, SFML, Thor"
         />
+        <Article
+          title={t(locale, "personal_project_node_race_title")}
+          subtitle={t(locale, "personal_project_node_race_subtitle")}
+          content={t(locale, "personal_project_node_race_content")}
+          buttonText={t(locale, "personal_project_node_race_buttonText")}
+          href={t(locale, "personal_project_node_race_href")}
+          githubUrl={t(locale, "personal_project_node_race_githubUrl")}
+          languages="Javascript, NodeJs, Jaws Js"
+        />
       </ArticlesContainer>
     );
   }
--- src/client/app/Translations.js
+++ src/client/app/Translations.js
@@ -26,60 +26,76 @@ const translations = {
     student_project_title: "Projets Etudiants",
     personal_project_title: "Projets Personnels",
     contribution_title: "Contribution",
+
     student_project_rep_code_title: "Project étudiant de traitement d'images",
     student_project_rep_code_subtitle: "Librairie 2D de génération d'images",
     student_project_rep_code_content: "Premier project en dut traitant de traitement d'images. Le but ici était de se perfectionner sur l'apprentisage du C++(11) et de toucher des concepts du developpement 2D : la structure des données des images, les transformation de bases, réalisation d'algorithme de traitement simple. Bref un des premiers de mes travaux abouti dans mon cursus",
     student_project_rep_code_buttonText: "Voir le repo",
     student_project_rep_code_href: "http://jquery-custom-scrollbar.rocketmind.pl/images/lena.png",
     student_project_rep_code_githubUrl: "https://github.com/guillaume-gomez/Rep-Code",
+
     student_project_nf11_title: "Production d'un compilateur Logo",
     student_project_nf11_subtitle: "Travaux pratiques sur la théorie des langages",
     student_project_nf11_content: "Le but de se projet était de pratiquer fondamentaux de la théorie des langages. Ce projet contenait un squelette avec un analyseur syntaxique ainsi qu'un outil de build(un projet netbeans). Le projet m'a permis de comprendre les notions vues en cours de maniere ludique(on peut faire pleins de dessins rapidement en Logo :))",
     student_project_nf11_buttonText: "Voir le repo",
     student_project_nf11_href: "https://raw.githubusercontent.com/guillaume-gomez/NF11-project/master/example.png?raw=true",
     student_project_nf11_githubUrl: "https://github.com/guillaume-gomez/NF11-project",
+
     student_project_nf29_title: "Redaction d'un convertiseur de MindMapping vers Hdoc",
     student_project_nf29_subtitle: "Initiation à Ingénierie documentaire",
     student_project_nf29_content: "Dans le cadre d'une U.V traitant de l'Ingénierie documentaire. Ce cours permettait de travailler sur des problématique de conversion de data pour centraliser, formaliser, et sauvegarder de l'information. Le convertiseur en question permet de transformer une mind map(carte heuristique) vers un format permettant de rédiger du contenu et de l'exporter dans différents format(doc, pdf, web, presentation, ...)",
     student_project_nf29_buttonText: "Voir le repo",
     student_project_nf29_href: "res/NF29.png",
     student_project_nf29_githubUrl: "https://github.com/guillaume-gomez/NF29-XmlConverter",
+
     participation_project_LD34_title: "Ludum Dare 34",
     participation_project_LD34_subtitle: "Theme : Two Button Controls",
     participation_project_LD34_content: "J'ai participé à la gestion des ennemies(graphisme, mouvement, IA), qui avait été commencé. Malheureuseument le projet n'a jamais été terminé.",
     participation_project_LD34_buttonText: "Lien vers le repo",
     participation_project_LD34_href: "res/LD34.png",
     participation_project_LD34_githubUrl: "https://github.com/RMEx/LD34",
+
     participation_project_camlCompiler_title: "Caml Compiler",
     participation_project_camlCompiler_subtitle: "Compilateur d'ocaml en C++",
     participation_project_camlCompiler_content: "Ce projet étudiant est principalement programmé par Adrien Jarretier. Je suis intervenu sur le developpement de tests unitaires et intégration d'un hook pour lancer les tests à chaque pull request avec tracis.ci",
     participation_project_camlCompiler_buttonText: "Lien vers le repo",
     participation_project_camlCompiler_href: "https://www.lexifi.com/files/images/csml_winforms_client.jpg",
     participation_project_camlCompiler_githubUrl: "https://github.com/AdrienJarretier/CamlCompiler",
+
     personal_project_LD35_title: "Ludum Dare 35",
     personal_project_LD35_subtitle: "Theme : Shapeshift",
     personal_project_LD35_content: "Ma première participation au ludum dare, et ma dernière utilisation du moteur Jaws js. L'idée était de faire un jeu dans lequel la transformation physique du personnage aurait un impact sur le gameplay(gravité, colission, vitesse...)",
     personal_project_LD35_buttonText: "Jouer",
     personal_project_LD35_href: "https://raw.githubusercontent.com/guillaume-gomez/LD35/master/css/screen-shot.png",
     personal_project_LD35_githubUrl: "https://guillaume-gomez.github.io/LD35",
+
     personal_project_LD36_title: "Ludum Dare 36",
     personal_project_LD36_subtitle: "Theme : Ancient Technology",
     personal_project_LD36_content: "Ma seconde participation, et ma premiere avec Phaser. J'ai aussi ajouté une chaine de build pour coder en ES6. J'ai voulu traduire l'idée que la Technologie avance parfois tellement vite que l'on perd la connaissance d'outil qui ne sont pas si antique que ca. J'ai tenté de rendre hommage à certains outils issu des 80's",
     personal_project_LD36_buttonText: "Jouer",
     personal_project_LD36_href: "https://raw.githubusercontent.com/guillaume-gomez/LD36/master/preview.png",
     personal_project_LD36_githubUrl: "https://guillaume-gomez.github.io/LD36/build/",
+
     personal_project_LD37_title: "Ludum Dare 37",
     personal_project_LD37_subtitle: "Theme : One room",
     personal_project_LD37_content: "Dernière participation de l'année 2016. Toujours en phaser avec une chaine de build pour coder en ES6. Le jeu genere une salle randomisé, les ennemies peuvent pousser les blocs de la salle mais pas le hero. Pour gagner le personnage doit soit sortir de la salle(en laissant entrer les enemies), soit tous les eliminer.",
     personal_project_LD37_buttonText: "Jouer",
     personal_project_LD37_href: "https://raw.githubusercontent.com/guillaume-gomez/LD37/master/preview.png",
     personal_project_LD37_githubUrl: "https://guillaume-gomez.github.io/LD37/build/",
+
     personal_project_projet_math_title: "Math game",
     personal_project_projet_math_subtitle: "Nom définitif à definir :)",
     personal_project_projet_math_content: "Le but était ici de faire un jeu vidéo basé sur l'utilisation de concepts mathématiques. Le tout en ajoutant des mécaniques de plateforme 2D. Ce projet est toujours en cours d'amélioration.",
     personal_project_projet_math_buttonText: "Voir le repo",
     personal_project_projet_math_href: "https://raw.githubusercontent.com/guillaume-gomez/MathGame/master/resReadme/in-game3.png",
-    personal_project_projet_math_githubUrl: "https://guillaume-gomez.github.io/MathGame"
+    personal_project_projet_math_githubUrl: "https://github.com/guillaume-gomez/MathGame",
+
+    personal_project_node_race_title: "NodeRace",
+    personal_project_node_race_subtitle: "Multiplayer game",
+    personal_project_node_race_content: "L'objectif de ce projet était principalement instructif. L'idée etait donc de developper en NodeJs une application web dynamique. A travers le concept de jeu, ce projet a permis d'entrevoir les problématiques de base des jeux en ligne(synchronisation, optimisation des messages, prédiction des données...",
+    personal_project_node_race_buttonText: "Voir le repo",
+    personal_project_node_race_href: "https://raw.githubusercontent.com/guillaume-gomez/NodeRace/master/preview.png",
+    personal_project_node_race_githubUrl: "https://github.com/guillaume-gomez/NodeRace"
   },
   "en" : {
     a: "at",
@@ -160,7 +176,13 @@ const translations = {
     personal_project_projet_math_content: "",
     personal_project_projet_math_buttonText: "go to repository",
     personal_project_projet_math_href: "https://raw.githubusercontent.com/guillaume-gomez/MathGame/master/resReadme/in-game3.png",
-    personal_project_projet_math_githubUrl: "https://guillaume-gomez.github.io/MathGame"
+    personal_project_projet_math_githubUrl: "https://github.com/guillaume-gomez/MathGame",
+    personal_project_node_race_title: "NodeRace",
+    personal_project_node_race_subtitle: "Multiplayer game",
+    personal_project_node_race_content: "",
+    personal_project_node_race_buttonText: "",
+    personal_project_node_race_href: "https://raw.githubusercontent.com/guillaume-gomez/NodeRace/master/preview.png",
+    personal_project_node_race_githubUrl: "https://github.com/guillaume-gomez/NodeRace"
   }
 }
 
