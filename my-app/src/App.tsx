8134d0feabdb351707ad9cfb8b3a2fe3302d5129
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-10 00:31:23 +0100

    update style

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,28 +11,27 @@ function App() {
   const { t } = useTranslation();
   return (
     <div className="App">
-     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Guillaume Gomez</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">{t("header.cv")}</Nav.Link>
-          <NavDropdown title={t('header.work')} id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/2.1">{t("header.personnal-projects")}</NavDropdown.Item>
-            <NavDropdown.Item href="#action/2.2">{t("header.contribution")}</NavDropdown.Item>
-            <NavDropdown.Item href="#action/2.3">{t("header.student-projects")}</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link eventKey={2} href="https://www.linkedin.com/in/guillaume-gomez-252a14a3">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </Nav.Link>
-          <Nav.Link eventKey={2} href="https://github.com/guillaume-gomez">
-            <FontAwesomeIcon icon={faGithub} />
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar collapseOnSelect sticky="top" expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Guillaume Gomez</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#features">{t("header.skills")}</Nav.Link>
+            <Nav.Link href="#features">{t("header.experience")}</Nav.Link>
+            <Nav.Link href="#features">{t("header.portfolio")}</Nav.Link>
+            <Nav.Link href="#features">{t("header.resume")}</Nav.Link>
+            <Nav.Link href="#features">{t("header.contact")}</Nav.Link>
+            <span style={{border: "1px solid var(--secondary)", margin: "10px"}}>
+            </span>
+            <Nav.Link eventKey={2} href="https://www.linkedin.com/in/guillaume-gomez-252a14a3">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="https://github.com/guillaume-gomez">
+              <FontAwesomeIcon icon={faGithub} />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
