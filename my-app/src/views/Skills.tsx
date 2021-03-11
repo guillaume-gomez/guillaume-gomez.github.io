f58a232bf66aa8b9902c73d6a06a5732d739318c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-11 14:26:32 +0100

    add skills

--- my-app/src/views/Skills.tsx
+++ my-app/src/views/Skills.tsx
@@ -14,14 +14,28 @@ function Skills() {
     <section className="skills-content">
       <h2 className="skills-title">{t("skills.skills")}</h2>
       
-      <div>
-        <h3>Front End</h3>
-          
-        <ProgressBar value={35} label="HTML5/CSS" />
-        <ProgressBar value={35} label="Javascript (ES6)" />
-        <ProgressBar value={35} label="TypeScript" />
-        <ProgressBar value={35} label="React" />
-        
+      <div className="skills-stats">
+        <div className="skills-progress-bars">
+          <h3>Front-End</h3>
+          <ProgressBar value={75} label="HTML5/CSS" />
+          <ProgressBar value={90} label="React 16.8" />
+          <ProgressBar value={40} label="JQuery" />
+          <h3>Back-End</h3>
+          <ProgressBar value={70} label="Ruby on Rails" />
+          <ProgressBar value={40} label="Node js" />
+          <ProgressBar value={80} label="GraphQL" />
+          <h3>Languages</h3>
+          <ProgressBar value={75} label="Javascript (ES6)" />
+          <ProgressBar value={85} label="TypeScript" />
+          <ProgressBar value={70} label="Ruby" />
+          <ProgressBar value={60} label="C++" />
+          <ProgressBar value={50} label="Rust" />
+          <ProgressBar value={50} label="Elixir" />
+          <ProgressBar value={45} label="Erlang" />
+        </div>
+        <div className="skills-pie-chart">
+          <h3> My pie charts</h3>
+        </div>
       </div>
     </section>
   );
