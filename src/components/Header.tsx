854d5ae3885a1f14ad62cfd192494741cbf0a7c7
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-22 20:54:08 +0100

    add languages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import i18n from '../i18n';
+import { withTranslation } from 'react-i18next';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
@@ -7,12 +11,21 @@ import { LINKEDIN, GITHUB } from "../constants";
 import "./Header.css";
 
 function Header() {
+   const { t } = useTranslation();
+  function onChangeLanguage(e: any) {
+    i18n.changeLanguage(e.target.value)
+  }
   return (
     <div className="header-container">
       <div className="header-name">
         <p>Guillaume Gomez</p>
       </div>
       <div className="header-links">
+      <select onChange={onChangeLanguage}>
+        <option value="fr">{t("header.french")}</option>
+        <option value="en">{t("header.english")}</option>
+      </select>
+      <span className="header-separator"></span>
         <a href={LINKEDIN}>
           <FontAwesomeIcon icon={faLinkedin} size="lg" />
         </a>
