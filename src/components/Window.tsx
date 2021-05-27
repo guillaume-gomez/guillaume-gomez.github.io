d1186701e88f5dfc6900c262a7e2c94c21251593
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-05-27 10:12:22 +0200

    add windows panel

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import "./Window.css";
+
 
 interface WindowInterface {
   width?: number | string;
@@ -10,13 +13,22 @@ interface WindowInterface {
 
 function Window({width = 0, height = 0, backgroundColor, children} : WindowInterface) {
   return (
-    <div className="window-content scale-up-center" style={{
-      borderRadius: 17,
-      width,
-      height,
-      backgroundColor
-      }}>
-      {children}
+    <div className="scale-up-center">
+      <div style={{ background: "#d6d6d6", height: "30px", width: "100%", borderRadius: "10px 10px 0 0"}}>
+        <div style={{ gap: "5px", display: "flex", flexDirection: "row-reverse", alignItems: "center", width:'98%', height:'100%'}}>
+          <FontAwesomeIcon icon={faCircle} size="xs" color="#ff6159"/>
+          <FontAwesomeIcon icon={faCircle} size="xs" color="#ffc130"/>
+          <FontAwesomeIcon icon={faCircle} size="xs" color="#2acd43"/>
+        </div>
+      </div>
+      <div className="window-content" style={{
+        borderRadius: "0 0 10px 10px",
+        width,
+        height,
+        backgroundColor
+        }}>
+        {children}
+      </div>
     </div>
   );
 }
