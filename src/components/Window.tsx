001b1e5a1644efde3558d23efc980992c905c385
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-05-30 01:57:50 +0200

    design v3

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -13,7 +13,7 @@ interface WindowInterface {
 
 function Window({width = 0, height = 0, backgroundColor, children} : WindowInterface) {
   return (
-    <div className="scale-up-center">
+    <div className="scale-up-center" style={{width: width, height: height}}>
       <div style={{ background: "#d6d6d6", height: "30px", width: "100%", borderRadius: "10px 10px 0 0"}}>
         <div style={{ gap: "5px", display: "flex", flexDirection: "row-reverse", alignItems: "center", width:'98%', height:'100%'}}>
           <FontAwesomeIcon icon={faCircle} size="xs" color="#ff6159"/>
--- src/views/WelcomePage.tsx
+++ src/views/WelcomePage.tsx
@@ -27,32 +27,36 @@ function WelcomePage({refTarget} : WelcomePageInterface) {
 
   return (
     <section className="welcome-page-content first-gradient">
-      <div className="welcome-page-top">
-        <h1 className="title-relative"> <span className="about-me-wave-hand">👋</span><strong className="text-primary">Guillaume Gomez</strong></h1>
-        <h2 className="title-relative"> <span className="background--magical">{t("welcome-page.job-title")}</span> </h2>
-      </div>
-      <div className="welcome-page-windows">
-        <div className="welcome-page-terminal">
-          {
-            displayTerminal &&
-            <Window width={600} height={300} backgroundColor="black">
-              <Terminal />
-            </Window>
-          }
+      <div className="grid-container">
+        <div className="Title" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+          <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem"}}>
+            <h2 className="title-relative" style={{display: "flex", alignSelf:"start"}}>{t("welcome-page.i-am")}</h2>
+            <h1 className="title-relative"><strong className="text-primary">Guillaume Gomez</strong></h1>
+            <div className="welcome-page-bottom" style={{padding: "5rem"}} onClick={() => refTarget!.current!.scrollIntoView({behavior: "smooth"}) }>
+              <motion.div whileHover={{ translateY: 5, transition: { duration: 0.5, repeat: Infinity } }}>
+                <MacMouse fill="var(--white)" style={{width: "60px"}}/>
+              </motion.div>
+            </div>
+          </div>
         </div>
-        <div className="welcome-page-trello">
-          {
-            displayTrello &&
-            <Window width={600} height={425} backgroundColor="black">
-              <Trello/>
-            </Window>
-          }
+        <div className="TextFullStack" style={{gridColumnStart: 2, gridColumnEnd: 7, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+          <h3>Full stack developper</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
-      </div>
-      <div className="welcome-page-bottom" onClick={() => refTarget!.current!.scrollIntoView({behavior: "smooth"}) }>
-        <motion.div whileHover={{ translateY: 5, transition: { duration: 0.5, repeat: Infinity } }}>
-          <MacMouse fill="var(--white)" style={{width: "60px"}}/>
-        </motion.div>
+        <div className="terminal" style={{ gridColumnStart: 8, gridColumnEnd: 12,  display: "flex", flexDirection: "column", justifyContent: "center"}}>
+          <Window width={600} height={300} backgroundColor="black">
+            <Terminal />
+          </Window>
+        </div>
+        <div className="trello" style={{ gridColumnStart: 2, gridColumnEnd: 7}}>
+          <Window width={800} height={450} backgroundColor="black">
+            <Trello/>
+          </Window>
+        </div>
+        <div className="textCTO" style={{gridColumnStart: 8, gridColumnEnd: 12, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+          <h3>But a CTO too</h3>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
       </div>
     </section>
   );
