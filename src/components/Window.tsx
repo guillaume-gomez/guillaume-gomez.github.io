9e4fad94277e654567ef1393dacd2d6ff09c8e6d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-05-30 14:44:22 +0200

    improve media query about me page

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -5,15 +5,14 @@ import "./Window.css";
 
 
 interface WindowInterface {
-  width?: number | string;
-  height?: number | string;
+  minHeight?: number | string;
   backgroundColor: string;
   children?: React.ReactNode;
 }
 
-function Window({width = 0, height = 0, backgroundColor, children} : WindowInterface) {
+function Window({minHeight = 0, backgroundColor, children} : WindowInterface) {
   return (
-    <div className="scale-up-center" style={{width: width, height: height}}>
+    <div className="scale-up-center" style={{width: "100%", minHeight }}>
       <div style={{ background: "#d6d6d6", height: "30px", width: "100%", borderRadius: "10px 10px 0 0"}}>
         <div style={{ gap: "5px", display: "flex", flexDirection: "row-reverse", alignItems: "center", width:'98%', height:'100%'}}>
           <FontAwesomeIcon icon={faCircle} size="xs" color="#ff6159"/>
@@ -23,8 +22,8 @@ function Window({width = 0, height = 0, backgroundColor, children} : WindowInter
       </div>
       <div className="window-content" style={{
         borderRadius: "0 0 10px 10px",
-        width,
-        height,
+        width: "100%",
+        height: "calc(100% - 30px)",
         backgroundColor
         }}>
         {children}
--- src/views/WelcomePage.tsx
+++ src/views/WelcomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef} from 'react';
 import { useTranslation } from 'react-i18next';
 import { motion } from "framer-motion";
 import { ReactComponent as MacMouse } from "../macintosh-mouse.svg";
@@ -11,12 +11,9 @@ import Terminal from "../components/Terminal";
 import Window from "../components/Window";
 import Trello from "../components/Trello";
 
-interface WelcomePageInterface {
-  refTarget: React.RefObject<HTMLSpanElement>
-}
-
-function WelcomePage({refTarget} : WelcomePageInterface) {
+function WelcomePage() {
   const { t } = useTranslation();
+  const toTexts = useRef<HTMLDivElement>(null);
   const [displayTerminal, setDisplayTerminal] = useState<boolean>(false);
   const [displayTrello, setDisplayTrello] = useState<boolean>(false);
   
@@ -28,32 +25,32 @@ function WelcomePage({refTarget} : WelcomePageInterface) {
   return (
     <section className="welcome-page-content first-gradient">
       <div className="grid-container">
-        <div className="Title" style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-          <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "1rem"}}>
-            <h2 className="title-relative" style={{display: "flex", alignSelf:"start"}}>{t("welcome-page.i-am")}</h2>
-            <h1 className="title-relative"><strong className="text-primary">Guillaume Gomez</strong></h1>
-            <div className="welcome-page-bottom" style={{padding: "5rem"}} onClick={() => refTarget!.current!.scrollIntoView({behavior: "smooth"}) }>
+        <div className="title">
+          {<div style={{display: "flex", flexDirection: "column", alignItems: "center", border: "2px solid yellow", gap: "1rem", padding: "5rem"}}>
+            <h2 style={{display: "flex", alignSelf:"start", border: "2px solid grey"}}>{t("welcome-page.i-am")}</h2>
+            <h1 ><strong className="text-primary">Guillaume Gomez</strong></h1>
+            <div className="welcome-page-bottom" style={{padding: "5rem"}} onClick={() => toTexts!.current!.scrollIntoView({behavior: "smooth"}) }>
               <motion.div whileHover={{ translateY: 5, transition: { duration: 0.5, repeat: Infinity } }}>
                 <MacMouse fill="var(--white)" style={{width: "60px"}}/>
               </motion.div>
             </div>
-          </div>
+          </div>}
         </div>
-        <div className="TextFullStack" style={{gridColumnStart: 2, gridColumnEnd: 7, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+        <div ref={toTexts} className="textFullStack">
           <h3>Full stack developper</h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
-        <div className="terminal" style={{ gridColumnStart: 8, gridColumnEnd: 12,  display: "flex", flexDirection: "column", justifyContent: "center"}}>
-          <Window width={600} height={300} backgroundColor="black">
+        <div className="terminal">
+          <Window backgroundColor="black" minHeight={325}>
             <Terminal />
           </Window>
         </div>
-        <div className="trello" style={{ gridColumnStart: 2, gridColumnEnd: 7}}>
-          <Window width={800} height={450} backgroundColor="black">
+        <div className="trello">
+          <Window backgroundColor="none">
             <Trello/>
           </Window>
         </div>
-        <div className="textCTO" style={{gridColumnStart: 8, gridColumnEnd: 12, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+        <div className="textCTO">
           <h3>But a CTO too</h3>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
