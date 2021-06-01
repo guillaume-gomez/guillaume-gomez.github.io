095ce6c981fe650b39ddf9805510c0fe8cfa8300
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-06-02 00:11:12 +0200

    add animation

--- src/views/WelcomePage.tsx
+++ src/views/WelcomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef} from 'react';
 import { useTranslation } from 'react-i18next';
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { ReactComponent as MacMouse } from "../macintosh-mouse.svg";
 
 import "./WelcomePage.scss";
@@ -28,15 +29,17 @@ function WelcomePage({ toAboutMe, toTexts } : WelcomePageInterface) {
   return (
     <section className="welcome-page-content first-gradient">
       <Header refTarget={toAboutMe}/>
-      <div className="welcome-page-text">
-        <div style={{padding: "2rem"}}>
-          <h2>{t("welcome-page.i-am")}</h2>
-          <h1>
-            <strong className="text-primary">
-              Guillaume Gomez
-            </strong>
-          </h1>
-        </div>
+        <div className="welcome-page-text">
+        <FromUpToDown>
+          <div style={{padding: "2rem"}}>
+            <h2>{t("welcome-page.i-am")}</h2>
+            <h1>
+              <strong className="text-primary">
+                Guillaume Gomez
+              </strong>
+            </h1>
+          </div>
+        </FromUpToDown>
         <div className="welcome-page-bottom" style={{padding: "2rem", display: "flex", justifyContent: "center"}} onClick={() => toTexts!.current!.scrollIntoView({behavior: "smooth"}) }>
           <motion.div whileHover={{ translateY: 5, transition: { duration: 0.5, repeat: Infinity } }}>
             <MacMouse fill="var(--white)" style={{width: "60px"}}/>
@@ -48,3 +51,35 @@ function WelcomePage({ toAboutMe, toTexts } : WelcomePageInterface) {
 }
 
 export default WelcomePage;
+
+interface FromUpToDownInterface {
+  children: React.ReactNode;
+}
+
+
+function FromUpToDown({ children } : FromUpToDownInterface) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+      controls.start("move");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="visible"
+      transition={{ duration: 1.0, type: "spring", bounce: 0.25 }}
+      variants={{
+        visible: { y: -100, scale: 1.5 },
+        move: {y: 100, scale: 1.0 }
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
