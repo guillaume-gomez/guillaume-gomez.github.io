d29ea336db42b11ca26f829a77a97e4d879d0de3
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-06-06 21:25:51 +0200

    add animation in hobbies icons

--- src/views/Hobbies.tsx
+++ src/views/Hobbies.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRunning, faMusic, faFilm, faGamepad } from '@fortawesome/free-solid-svg-icons';
+
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 import "./Hobbies.css";
 
@@ -13,19 +16,27 @@ function Hobbies() {
       <h4 className="hobbies-title">{t("hobbies.hobbies")}</h4>
       <ul className="hobbies-icons">
         <li>
-          <FontAwesomeIcon icon={faRunning} size="lg" />
+          <FlipX>
+            <FontAwesomeIcon icon={faRunning} size="lg" />
+          </FlipX>
           <p>{t("hobbies.sports")}</p>
         </li>
         <li>
-          <FontAwesomeIcon icon={faMusic} size="lg"/>
-          <p>{t("hobbies.music")}</p>
+          <FlipX>
+            <FontAwesomeIcon icon={faMusic} size="lg"/>
+          </FlipX>
+          <p>{t("hobbies.music")}</p> 
         </li>
         <li>
-          <FontAwesomeIcon icon={faGamepad} size="lg"/>
+          <FlipX>
+            <FontAwesomeIcon icon={faGamepad} size="lg"/>
+          </FlipX>
           <p>{t("hobbies.video-games")}</p>
         </li>
         <li>
-          <FontAwesomeIcon icon={faFilm} size="lg"/>
+          <FlipX>
+            <FontAwesomeIcon icon={faFilm} size="lg"/>
+          </FlipX>
           <p>{t("hobbies.cinema")}</p>
         </li>
        </ul>
@@ -34,3 +45,33 @@ function Hobbies() {
 }
 
 export default Hobbies;
+
+interface FlipXInterface {
+  children: React.ReactNode;
+}
+
+function FlipX({ children } : FlipXInterface) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("move");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 1.0, ease: "easeInOut" }}
+      variants={{
+        hidden: { opacity: 1, scale: 1 },
+        move: { opacity: 1, scale: 1, rotateX: [0, 270, 360] }
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
