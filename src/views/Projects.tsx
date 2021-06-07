793eefa4c670006b547e7759da22129be4cc22ad
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-06-08 01:17:21 +0200

    scale up nb projects

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { uniq, orderBy } from "lodash";
 import { useTranslation } from 'react-i18next';
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import "./Projects.css";
 import ProjectsGrid from "../components/ProjectsGrid";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,6 +16,7 @@ const stepProjects = 10;
 
 function Projects({refTarget} : ProjectInterface) {
   const { t } = useTranslation();
+  const controls = useAnimation();
   const [filter, setFilter] = useState<string>("");
   const [itemsLoaded, setItemLoaded] = useState<number>(stepProjects);
   const [theme, setTheme] = useState<string>("");
@@ -47,11 +48,13 @@ function Projects({refTarget} : ProjectInterface) {
   useEffect(() => {
     const itemsLoaded = Math.min(projectsDataFiltered.length, stepProjects);
     setItemLoaded(itemsLoaded);
+    controls.start("scaleUp");
   }, [projectsDataFiltered]);
 
   function loadMore() {
     const itemsToAdd = Math.min(projectsData.length - itemsLoaded, stepProjects);
     setItemLoaded(itemsLoaded + itemsToAdd);
+    controls.start("scaleUp");
   }
 
   function onChangeTheme(theme: string) {
@@ -79,9 +82,19 @@ function Projects({refTarget} : ProjectInterface) {
                   {orderBy(tags).map(tag => <option className="custom-option" key={tag}>{tag}</option>)}
                 </select>
               </div>
-              <div className="projects-stat">
-                <strong>{` ${itemsLoaded} / ${projectsDataFiltered.length}`}</strong> {t("projects.projects")}
-              </div>
+                <motion.div className="projects-stat"
+                  animate={controls}
+                  initial="idle"
+                  variants={ {
+                    idle: {  scale: 1 },
+                    scaleUp: { scale: [1.1, 1.2, 1.3, 1.5, 1.3, 1.2, 1.1, 1.0] },
+                  }}
+                  transition={{ duration: 0.5, ease: "easeInOut" }}>
+                <strong>
+                  {` ${itemsLoaded} / ${projectsDataFiltered.length}`}
+                </strong>
+                </motion.div>
+                {t("projects.projects")}
              </div>
             <motion.div
               whileHover={{ translateY: -5, transition: { duration: 0.5, repeat: Infinity } }}
