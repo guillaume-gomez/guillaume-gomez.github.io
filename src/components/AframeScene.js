cc56264dade1965ba8f83fbff80c3733c8b14ff3
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-08-01 05:03:17 +0200

    render a scene properly

--- src/components/AframeScene.js
+++ src/components/AframeScene.js
@@ -5,19 +5,25 @@ import "./AframeScene.css";
 function AframeScene() {
   return (
     <a-scene embedded vr-mode-ui="enabled: false">
+      <a-assets>
+        {/*<img id="my-image" src={`${process.env.PUBLIC_URL}/amuse.jpg`} /> */}
+        <a-asset-item id="computer-obj" src={`${process.env.PUBLIC_URL}/3d-assets/mainframe.obj`}></a-asset-item>
+        <a-asset-item id="computer-mtl" src={`${process.env.PUBLIC_URL}/3d-assets/mainframe.mtl`}></a-asset-item>
+
+      </a-assets>
+      <a-camera camera="fov: 80;" id="camera" rotation-reader position="0 1.6 0" listener wasd-controls-enabled="false">
+</a-camera>
       <a-entity
-        geometry="primitive: box" 
-        position="-1 0.5 -3" 
-        rotation="0 45 0" 
-        material="color: #4CC3D9" />
-      <a-entity
-        geometry="primitive: sphere; radius: 1.25;" 
-        position="0 1.25 -5" 
-        material="color: #EF2D5E" />
-      <a-entity
-        geometry="primitive: cylinder; radius: 0.5, height: 1.5" 
-        position="1 0.75 -3" 
-        material="color: #FFC65D" />
+            animation="property: object3D.position.y; to: 1.0; dir: alternate; dur: 2000; loop: true"
+            obj-model="obj: #computer-obj; mtl: #computer-mtl;"
+            scale="0.5 0.5 0.5"
+            position="-10 0 -15"
+            rotation="0 90 0"
+   ></a-entity>
+       {/*<a-image src="#my-image"  scale="2 2 2"
+            position="0 0.5 -13"
+            rotation="-10 0 0"></a-image>
+     */}
     </a-scene>
   );
 }
