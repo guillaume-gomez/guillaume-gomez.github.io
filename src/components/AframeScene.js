442a9949bd8e8431a27c6c31ff64961b9de4584c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-08-07 00:12:44 +0200

    update frame scene

--- src/components/AframeScene.js
+++ src/components/AframeScene.js
@@ -4,22 +4,37 @@ import "./AframeScene.css";
 
 function AframeScene() {
   return (
-    <a-scene embedded vr-mode-ui="enabled: false">
+    <a-scene  loading-screen="dotsColor: red; backgroundColor: #062859" embedded vr-mode-ui="enabled: false">
       <a-assets>
-        {/*<img id="my-image" src={`${process.env.PUBLIC_URL}/amuse.jpg`} /> */}
+        {/*<img id="my-image" src={`${process.env.PUBLIC_URL}/terminal.jpg`} /> */}
         <a-asset-item id="computer-obj" src={`${process.env.PUBLIC_URL}/3d-assets/mainframe.obj`}></a-asset-item>
         <a-asset-item id="computer-mtl" src={`${process.env.PUBLIC_URL}/3d-assets/mainframe.mtl`}></a-asset-item>
-
+        <a-asset-item id="balloon" src={`${process.env.PUBLIC_URL}/3d-assets/ball.gltf`}></a-asset-item>
+         <a-asset-item id="cassette" src={`${process.env.PUBLIC_URL}/3d-assets/scene.gltf`}></a-asset-item>
       </a-assets>
       <a-camera camera="fov: 80;" id="camera" rotation-reader position="0 1.6 0" listener wasd-controls-enabled="false">
 </a-camera>
       <a-entity
-            animation="property: object3D.position.y; to: 1.0; dir: alternate; dur: 2000; loop: true"
+            animation="property: object3D.position.y; to: 0.5; dir: alternate; dur: 2000; loop: true"
             obj-model="obj: #computer-obj; mtl: #computer-mtl;"
             scale="0.5 0.5 0.5"
             position="-10 0 -15"
             rotation="0 90 0"
    ></a-entity>
+   <a-gltf-model src="#balloon"
+        animation="property: object3D.position.y; to: 0; dir: alternate; dur: 1500; loop: true"
+        scale="0.01 0.01 0.01"
+        position="4 -3.5 -7"
+        rotation="0 -90 0"
+      >
+      </a-gltf-model>
+       <a-gltf-model src="#cassette"
+        animation="property: object3D.position.y; to: -4.5; dir: alternate; dur: 2000; loop: true"
+        scale="1 1 1"
+        position="2 -5 -10"
+        rotation="0 0 0"
+      >
+      </a-gltf-model>
        {/*<a-image src="#my-image"  scale="2 2 2"
             position="0 0.5 -13"
             rotation="-10 0 0"></a-image>
