22cec9020642e4777b9064e2ee441189994dfee9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-08-11 19:10:59 +0200

    update scene

--- src/components/AframeScene.js
+++ src/components/AframeScene.js
@@ -13,7 +13,7 @@ function AframeScene() {
         <a-asset-item id="balloon" src={`${process.env.PUBLIC_URL}/3d-assets/ball.gltf`}></a-asset-item>
          <a-asset-item id="cassette" src={`${process.env.PUBLIC_URL}/3d-assets/cassette.gltf`}></a-asset-item>
       </a-assets>
-      <a-camera camera="fov: 80;" id="camera" look-controls orbit-controls="target: 0 0 -15; minDistance: 0.5; maxDistance: 180; initialPosition: 0 0 1.6" listener wasd-controls-enabled="false">
+      <a-camera camera="fov: 80;" id="camera" look-controls orbit-controls="target: 0 0 -15; minDistance: 0.5; maxDistance: 180; initialPosition: 0 0 -1.5" listener wasd-controls-enabled="false">
 </a-camera>
       <a-entity
             animation="property: object3D.position.y; to: 0.5; dir: alternate; dur: 2000; loop: true"
@@ -25,19 +25,19 @@ function AframeScene() {
    <a-gltf-model src="#balloon"
         animation="property: object3D.position.y; to: 0; dir: alternate; dur: 1500; loop: true"
         scale="0.01 0.01 0.01"
-        position="4 -3.5 -7"
+        position="5 -5.5 -12.5"
         rotation="0 -90 0"
       >
       </a-gltf-model>
        <a-gltf-model src="#cassette"
-        animation="property: object3D.position.y; to: -4.5; dir: alternate; dur: 2000; loop: true"
+        animation="property: object3D.position.y; to: -6; dir: alternate; dur: 2000; loop: true"
         scale="1 1 1"
-        position="2 -5 -10"
+        position="2 -6.5 -10"
         rotation="0 0 0"
       >
       </a-gltf-model>
        {/*<a-image src="#my-image"  scale="2 2 2"
-            position="0 0.5 -13"
+            position="0 -6.5 -13"
             rotation="-10 0 0"></a-image>
      */}
     </a-scene>
