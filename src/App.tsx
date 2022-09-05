9e1acce2e5cbc6886b29102ea76914af3d5b84c0
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-09-06 00:08:57 +0200

    change suspence to display footer and header at first

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useRef, lazy, Suspense } from 'react';
 import { renderToStaticMarkup } from "react-dom/server";
 import { isBrowser } from 'react-device-detect';
 //import SmoothScroll from "./components/animations/smoothScroll/SmoothScroll";
+import WelcomePage from "./views/WelcomePage";
+import Footer from "./views/Footer";
 import Cursor from "./components/Cursor";
 import { ReactComponent as Stripes } from "./stripes.svg";
 import './App.css';
@@ -12,10 +14,8 @@ const Hobbies = lazy(() => import ("./views/Hobbies"));
 const Experience = lazy(() => import ("./views/Experience"));
 const Skills = lazy(() => import ("./views/Skills"));
 const Projects = lazy(() => import ("./views/Projects"));
-const WelcomePage = lazy(() => import ("./views/WelcomePage"));
 const WelcomePageInfos = lazy(() => import ("./views/WelcomePageInfos"));
 const Education = lazy(() => import ("./views/Education"));
-const Footer = lazy(() => import("./views/Footer"));
 const renderLoader = () => <p style={{height: "100vh", textAlign: "center"}}>Loading</p>;
 
 function App() {
@@ -30,30 +30,30 @@ function App() {
     return (
       <div className="App">
         { isBrowser && <Cursor /> }
-        <Suspense fallback={renderLoader()}>
           <div className="App-container">
+            <Suspense fallback={renderLoader()}>
             <div className="stripes" style={{backgroundImage: `url('data:image/svg+xml;utf8, ${svgString}')`, display: "block"}} />
             <div className="container">
               <span ref={toHomepage}>
                 <WelcomePage toAboutMe={toAboutMe} toTexts={toTexts}/>
               </span>
-              <span ref={toTexts}>
-                <WelcomePageInfos/>
-              </span>
-              <span ref={toProject}>
-               <Projects refTarget={toAboutMe} />
-              </span>
-              <Experience />
-              <Education />
-              <span ref={toAboutMe} style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <AboutMe />
-                <Hobbies />
-              </span>
-              <Skills />
+                <span ref={toTexts}>
+                  <WelcomePageInfos/>
+                </span>
+                <span ref={toProject}>
+                 <Projects refTarget={toAboutMe} />
+                </span>
+                <Experience />
+                <Education />
+                <span ref={toAboutMe} style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
+                  <AboutMe />
+                  <Hobbies />
+                </span>
+                <Skills />
             </div>
+            </Suspense>
           </div>
           <Footer refTarget={toHomepage}/>
-        </Suspense>
       </div>
     );
   }
