d8ba03154904b0be36b231e17f9f1aac101d2c53
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-09-08 01:27:54 +0200

    display as link when custom button has link

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -9,6 +9,18 @@ interface CustomButtonInterface {
 }
 
 function CustomButton({ children, className, onClick, href }: CustomButtonInterface): React.ReactElement {
+  if(href) {
+    return (
+      <motion.a
+        className={className}
+        href={href}
+        whileHover={{ borderRadius: ["5px", "10px", "25px", "30px"], transition: { duration: 0.2 } }}
+      >
+        {children}
+      </motion.a>
+    )
+  }
+
   return (
     <motion.button
       className={className}
