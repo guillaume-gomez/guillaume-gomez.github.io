d25fa3d017e56c8e01a45c82b23ccc9ff9294939
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-05-23 10:51:16 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,35 @@
-import React, { useRef, lazy, Suspense } from 'react';
-import { renderToStaticMarkup } from "react-dom/server";
-import { isBrowser } from 'react-device-detect';
-//import SmoothScroll from "./components/animations/smoothScroll/SmoothScroll";
-import WelcomePage from "./views/WelcomePage";
-import Footer from "./views/Footer";
-import Cursor from "./components/Cursor";
-import { ReactComponent as Stripes } from "./stripes.svg";
-import './App.css';
-
-// lazy components
-const AboutMe = lazy(() => import ("./views/AboutMe"));
-const Hobbies = lazy(() => import ("./views/Hobbies"));
-const Experience = lazy(() => import ("./views/Experience"));
-const Skills = lazy(() => import ("./views/Skills"));
-const Projects = lazy(() => import ("./views/Projects"));
-const WelcomePageInfos = lazy(() => import ("./views/WelcomePageInfos"));
-const Education = lazy(() => import ("./views/Education"));
-const renderLoader = () => <p style={{height: "100vh", textAlign: "center"}}>Loading</p>;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const toProject = useRef<HTMLSpanElement>(null);
-  const toHomepage = useRef<HTMLSpanElement>(null);
-  const toAboutMe = useRef<HTMLSpanElement>(null);
-  const toTexts = useRef<HTMLSpanElement>(null);
+  const [count, setCount] = useState(0)
 
-  const svgString = encodeURIComponent(renderToStaticMarkup(<Stripes />));
-
-  function renderCommon() {
-    return (
-      <div className="App">
-        { isBrowser && <Cursor /> }
-          <div className="App-container">
-            <Suspense fallback={renderLoader()}>
-            <div className="stripes" style={{backgroundImage: `url('data:image/svg+xml;utf8, ${svgString}')`, display: "block"}} />
-            <div className="container">
-              <span ref={toHomepage}>
-                <WelcomePage toAboutMe={toAboutMe} toTexts={toTexts}/>
-              </span>
-                <span ref={toTexts}>
-                  <WelcomePageInfos/>
-                </span>
-                <span ref={toProject}>
-                 <Projects refTarget={toAboutMe} />
-                </span>
-                <Experience />
-                <Education />
-                <span ref={toAboutMe} style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-                  <AboutMe />
-                  <Hobbies />
-                </span>
-                <Skills />
-            </div>
-            </Suspense>
-          </div>
-          <Footer refTarget={toHomepage}/>
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    );
-  }
-
-  /*if(isBrowser) {
-    return (
-      <SmoothScroll>
-        {renderCommon()}
-      </SmoothScroll>
-    );
-  }*/
-
-  return renderCommon();
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
