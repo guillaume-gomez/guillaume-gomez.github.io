e52e1a70a315107688844f97737a16d31ac3fd0b
guillaume-gomez <guillaume.gomez846@gmail.com>
2025-06-02 16:40:14 +0200

    start project grid

---
+++ src/components/GridProject.tsx
@@ -0,0 +1,69 @@
+import "./GridProject.css"
+
+
+
+function GridProject() {
+  return (
+    <div className="gridProjectContainer h-screen bg-primary">
+      <div className="bigProject1">
+        
+      </div>
+      <div className="bigProject2">
+        
+      </div>
+      <div className="bigProject3">
+        
+      </div>
+      <div className="bigProject4">
+        
+      </div>
+      <div className="bigProject5">
+
+      </div>
+
+      <div className="other">
+          first
+      </div>
+
+      <div className="other">
+          second
+      </div>
+
+      <div className="other">
+          third
+      </div>
+
+      <div className="other">
+          four
+      </div>
+
+
+      <div className="other">
+          five
+      </div>
+
+      <div className="other">
+          six
+      </div>
+
+      <div className="other">
+          seven
+      </div>
+
+      <div className="other">
+          eight
+      </div>
+
+      <div className="other">
+          nine
+      </div>
+
+      <div className="other">
+          ten
+      </div>
+    </div>
+  );
+
+}
+
+export default GridProject;
