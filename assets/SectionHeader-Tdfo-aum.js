b99f9ee32cc89281eda252f1af4c188628d04388
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-05-18 13:14:00 +0200

    Updates

---
+++ assets/SectionHeader-Tdfo-aum.js
@@ -0,0 +1,1 @@
+import{n as e}from"./index-9Kkq7RZq.js";function s({text:r}){return e.jsxs("div",{className:"section-header-container",children:[e.jsx("h2",{children:r}),e.jsx("hr",{})]})}export{s as S};
